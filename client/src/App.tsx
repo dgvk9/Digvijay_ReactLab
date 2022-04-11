681e7f32a52deb2dffc5e6c5b9233085c9eac714
Digvijay <dgvj.kmr@gmail.com>
2022-04-11 23:35:18 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ExpenseTracker from './components/ExpenseTracker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+        <ExpenseTracker />
+    </div>
+  );
+}
+
+export default App;
